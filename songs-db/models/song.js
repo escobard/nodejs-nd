5a7a008fd5a5f57196091452036e85d6775de1c0
e88626 <descobar@atb.com>
2019-04-24 15:01:28 -0600

    course3 - chore: added songs model

---
+++ songs-db/models/song.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const { STRING, UUID } = require('sequelize');
+
+const SongsModel = {
+    
+    // this should be a UUID, which is a randomly generated ID
+    id: {
+        // determines data tye
+        type: UUID,
+        // creates no null constraint
+        allowNull: false,
+        // determines that this is the primary key of the table
+        primaryKey: true,
+        // sets the default value of this column
+        defaultValue: UUIDV4
+    },
+    artist: {
+        type: STRING(50),
+        allowNull: false,
+    },
+    album: {        
+        type: STRING(50),
+        allowNull: false,
+    },
+    song: {        
+        type: STRING(50),
+        allowNull: false,
+    }
+}
+
+module.exports = SongsModel;
