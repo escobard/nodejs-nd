8849d7604c1edfac37e4e6881920d40d1401e9d0
e88626 <descobar@atb.com>
2019-04-24 15:15:01 -0600

    course3 - feat: connected model to songs-db constructor class

--- songs-db/songs-db.js
+++ songs-db/songs-db.js
@@ -1,6 +1,8 @@
 'use-strict'
 
-const path = require('path';)
+const path = require('path'), 
+sequelize = require('sequelize'),
+SongModel = require('./models/song')
 
 class SongsDb{
     constructor(dbFile){
@@ -10,7 +12,11 @@ class SongsDb{
 
         // this specifies the file that contains the SQL storage
         this._storage = path.join(__dirname, dbFile);
+
+        // contains the sequelize instance
         this._sequelize = null;
+
+        // contains the song model
         this._song = null;
     }
 
@@ -22,7 +28,26 @@ class SongsDb{
 
             // establishes the DB language, in this case sqlite
             dialect: 'sqlite',
+
+            // sets the file / URL for the instance
+            storage: this_storage
         })
+        
+        // defines the model from the file
+        const Song = this_sequelize.define('song', SongsModel);
+        
+        // sets the new 'song' model to a .this method
+        this._song = Song
+
+        // not qite sure what .sync is, but basically returns the new sequelize instance
+        // with established configurations
+        return this._sequelize.sync();
+    }
+
+    static async(dbFile = 'songs.db'){
+        const db = new SongsDb(dbFile)
+        await db._init()
+        return db
     }
 }
 
